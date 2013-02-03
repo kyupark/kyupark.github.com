8852b2245f6e6adc36e1ca8157c66652e8c34e84
Kyu Park <park@kyu.co>
2013-02-02 20:36:30 -0500

    1st footer

---
+++ private/footer.php
@@ -0,0 +1,21 @@
+<?php
+global $cdn;
+?>
+<div id="footer">
+
+        <div id="footerInner">
+        <?php
+        if($_SERVER['REQUEST_URI'] != "/odk-plus")
+        {
+        ?>
+<div id="companion_footer">
+<iframe id='a468d13c' name='a468d13c' src='http://ads.ondemandkorea.com/www/delivery/afr.php?zoneid=39&amp;target=_blank&amp;cb=%n&amp;ct0=%c' frameborder='0' scrolling='no' width='728' height='90'><a href='http://ads.ondemandkorea.com/www/delivery/ck.php?n=ad72c277&amp;cb=%n' target='_blank'><img src='http://ads.ondemandkorea.com/www/delivery/avw.php?zoneid=39&amp;cb=%n&amp;n=ad72c277&amp;ct0=%c' border='0' alt='' /></a></iframe>
+</div>
+        <?php
+        }
+        ?>
+
+        <br><br>
+        <div class="clear"></div>
+                <div id="footerMenu">
+                <?php global $lang;global $langvar; global $odk;?>
