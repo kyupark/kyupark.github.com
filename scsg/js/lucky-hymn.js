dfef934fabc95603b453668fe4a669b1bbe7e6c5
Kyu Park <park@kyu.co>
2013-03-12 14:41:54 -0400

    fix feeling lucky url

--- scsg/js/lucky-hymn.js
+++ scsg/js/lucky-hymn.js
@@ -1,5 +1,5 @@
 function lucky(num) {    
-	var url = ' http://www.google.com/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&btnI=1&q=새찬송가+';
+	var url = ' http://www.google.com/search?btnI&q=새찬송가+';
 	var text = String(num).split('-').join('+').split('&').join('+');
 	url = url + text + "장";
 	return url;
