74119da9dafcc12a4bb14f54ebddefd8ba2dfd56
Kyu Park <park@kyu.co>
2013-03-12 13:40:03 -0400

    number of result in google to 1

--- scsg/js/lucky-hymn.js
+++ scsg/js/lucky-hymn.js
@@ -1,5 +1,5 @@
 function lucky(num) {    
-	var url = 'http://www.google.com/search?btnI=I&sourceid=navclient&ie=UTF-8&oe=UTF-8&q=새찬송가+';
+	var url = 'http://www.google.com/search?btnI=I&num=1&sourceid=navclient&ie=UTF-8&oe=UTF-8&q=새찬송가+';
 	var text = String(num).split('-').join('+').split('&').join('+');
 	url = url + text + "+장";
 	return url;
